960cfb139c7ed297ef2ed97c475d300859ff918e
Jalile59 <jal.djellouli@gmail.com>
2018-05-10 19:57:19 +0200

    add function delete figure

--- src/DJ/viewBundle/Controller/FigureController.php
+++ src/DJ/viewBundle/Controller/FigureController.php
@@ -7,12 +7,26 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FigureController extends Controller {
     
-    public function delfigureAction(Request $request, $id)
+    public function delfigureAction($id)
     {
         
-        $em = $this->getDoctrine()->getRepository('DJviewBundle:Figures')->find($id);
+        $em = $this->getDoctrine()->getManager();
+        
+        $figure = $em->find('DJviewBundle:Figures', $id);
         
         
+        if ($figure){
+                                
+            $em->remove($figure);
+            
+            $em->flush();
+                       
+        }else{
+            
+            throw $this->createNotFoundException('Cette figure n\'existe pas');
+        }
+                
+       return $this->redirectToRoute('d_jview_homepage');
         
         
     }
