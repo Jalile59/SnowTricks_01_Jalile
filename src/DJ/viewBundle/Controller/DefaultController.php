1f8814f40c88320d75028d4f5c5997472fb15491
Jalile59 <jal.djellouli@gmail.com>
2018-05-11 11:29:31 +0200

    pointsave

--- src/DJ/viewBundle/Controller/DefaultController.php
+++ src/DJ/viewBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {   
-        
+        //test
         $figure = $this->getDoctrine()->getRepository('DJviewBundle:Figures')->myfindall();
         
 //        $test = $figure->getPicture();
