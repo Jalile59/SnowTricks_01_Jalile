fbb201d8e2875c71f91c0ae607ab278be019d97b
Jalile59 <jal.djellouli@gmail.com>
2018-06-30 14:52:06 +0200

    Fixture update doctrine
    add linkvideo for eatch figure.

--- src/DJ/viewBundle/DataFixtures/ORM/DJFixtures.php
+++ src/DJ/viewBundle/DataFixtures/ORM/DJFixtures.php
@@ -22,7 +22,18 @@ class DJFixtures extends Fixture
             10 => 'salom air'
         ];
         
+        $videolink= [   
+                    1 =>'<iframe width="560" height="315" src="https://www.youtube.com/embed/8VsIZiM_Y6c?start=300" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+                    2 =>'<iframe width="560" height="315" src="https://www.youtube.com/embed/8VsIZiM_Y6c?start=200" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+                    3=>'<iframe width="560" height="315" src="https://www.youtube.com/embed/8VsIZiM_Y6c?start=100" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+            
+            
+        ];
+        
         $sizeNameTricks = count($nameTricks);
+        $sizevideoTricks = count($videolink);
+        
+        echo $sizevideoTricks;
         
         $figureDescription = "Une rotation peut être frontside ou backside : une rotation frontside correspond à une rotation orientée vers la carre backside. Cela peut paraître incohérent mais l'origine étant que dans un halfpipe ou une rampe de skateboard, une rotation frontside se déclenche naturellement depuis une position frontside (i.e. l'appui se fait sur la carre frontside), et vice-versa. Ainsi pour un rider qui a une position regular (pied gauche devant), une rotation frontside se fait dans le sens inverse des aiguilles d'une montre. Une rotation peut être agrémentée d'un grab, ce qui rend le saut plus esthétique mais aussi plus difficile car la position tweakée a tendance à déséquilibrer le rideur et désaxer la rotation. De plus, le sens de la rotation a tendance à favoriser un sens de grab plutôt qu'un autre. Les rotations de plus de trois tours existent mais sont plus rares, d'abord parce que les modules assez gros pour lancer un tel saut sont rares, et ensuite parce que la vitesse de rotation est tellement élevée qu'un grab devient difficile, ce qui rend le saut considérablement moins esthétique.";
         // create 20 products! Bam!
@@ -53,7 +64,18 @@ class DJFixtures extends Fixture
             $picture->setPictureCreatedate(new \DateTime());
             $picture->setPictureLink($nameTricks[$i].".jpg");
             
+            for($l = 1; $l <= $sizevideoTricks; $l++){
+                
+                
+                echo 'indexlink '.$l;
+                 
+                $video = new DJ\viewBundle\Entity\Videos();
+                $video->setVideolink($videolink[$l]);
+                $video->setFigurevideo($figure);
+                
+                $manager->persist($video);
             
+            }
 
             
             
@@ -66,10 +88,8 @@ class DJFixtures extends Fixture
             }
             
             $manager->persist($figure);
-
             
             $manager->persist($comment);
-
             $manager->persist($picture);
             
         }
