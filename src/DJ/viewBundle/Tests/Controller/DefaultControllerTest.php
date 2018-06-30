a537b4f8bf45afe0b5ef9036beda6cf274a2cb30
Jalile59 <jal.djellouli@gmail.com>
2018-06-30 18:33:31 +0200

    add test phpunit

--- src/DJ/viewBundle/Tests/Controller/DefaultControllerTest.php
+++ src/DJ/viewBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,17 +1,63 @@
 <?php
-
-namespace DJ\viewBundle\Tests\Controller;
-
+namespace Tests\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client     = null;    
+    private $login      = 'test_user_';
+    private $email      = 'test_email_';
+    private $password   = 'test_password';
+    
+    
+    public function setUp()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->client = static::createClient();
+        
+        $this->login .= md5(uniqid()); 
+        $this->email .= $this->login . '@test.com';
     }
+    
+    
+    public function testHomePage() {
+        $crawler = $this->client->request('GET', '/');
+        $this->assertContains('Bienvenue sur SnowTricks', $crawler->filter('h1')->getNode(0)->nodeValue);
+    }
+    
+    
+    public function testInscription() {
+                
+        $client     = static::createClient();        
+        $crawler    = $client->request('GET', '/inscription');        
+        $form       = $crawler->selectButton('Save')->form();        
+        
+        $form['dj_usersecuritybundle_user[username]']   = $this->login;
+        $form['dj_usersecuritybundle_user[userphoto]']  = __DIR__ . '/../../../web/images/020637f43136c6f7a642ebbdaafff4f3.jpg';
+        $form['dj_usersecuritybundle_user[password]']   = $this->password;
+        $form['dj_usersecuritybundle_user[mail]']       = $this->email;
+        
+        $crawler    = $client->submit($form);        
+        
+        $this->assertEquals('DJ\viewBundle\Controller\DefaultController::inscriptionAction', $client->getRequest()->attributes->get('_controller'));
+        
+    }
+    
+    /*
+    
+    public function testAddFigure() {
+                
+    }
+    
+    public function testDetailFigure() {
+        
+    }
+        
+    
+    public function testDeleteFigure() {
+        
+    }
+    */
+        
 }
